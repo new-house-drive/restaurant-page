35eca33da605aa82cd816a7d91096cd61d949d48
valerkin <lyudfaffnir@gmail.com>
2021-05-10 02:36:02 +0300

    💥 NEW: module content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('I <3 Nika!');
+'use strict'
+const Restaurant = () => {
+
+    
+
+    const addTitle = () => {
+        let contentElements = document.querySelector('.content')
+        let title = document.createElement('div')
+        title.className = 'title-element'
+
+        contentElements.appendChild(title)
+
+    }
+
+
+    return {
+        addTitle
+    }
+}
+
+const Module = (() => {
+    console.log('xxx')
+    Restaurant.addTitle()
+})()
