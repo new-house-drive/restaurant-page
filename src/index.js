b9e4c3a4f03973b73fb4a0ba7b96d515c9cf57c6
valera <lyudfaffnir@gmail.com>
2021-08-28 01:54:43 +0300

    add navbar buttons

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
-'use strict'
+"use strict";
+import "./style.css";
+
 const Restaurant = (() => {
-    // let content = document.getElementById('content')
-    
-    const addTitle = () => {    
-        // let title = document.createElement('div')
-        // title.classList.add('title-element')
-        // title.innerText = 'popa'
-        console.log('Hello!')
-        
-        // content.appendChild(title)
+  let content = document.getElementById("content");
 
+  const addNavbar = () => {
+    let navbarView = document.createElement("div");
+
+    navbarView.classList.add("navbar-view");
+
+    let navbarBtnNames = ["About us", "Menu", "Contact us"];
+    let navbarBtnIDs = ["about-us", "menu", "contact-us"];
+
+    for (let i = 0; i < 3; i++) {
+      let navbarButton = document.createElement("button");
+
+      navbarButton.classList.add("navbar-button");
+      navbarButton.innerText = navbarBtnNames[i];
+      navbarButton.id = navbarBtnIDs[i];
+
+      navbarView.appendChild(navbarButton);
     }
+    content.appendChild(navbarView);
+  };
 
-
-    return {
-        addTitle
-    }
-})()
+  return {
+    addNavbar,
+  };
+})();
 
 const Module = (() => {
-    Restaurant.addTitle()
-})()
+  Restaurant.addNavbar();
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,17 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
 };
