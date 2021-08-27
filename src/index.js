7c025bf55134cf5ac115e242367e8e92823b1f66
valera <lyudfaffnir@gmail.com>
2021-08-27 20:13:44 +0300

    add dist folder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 'use strict'
 const Restaurant = (() => {
-    let content = document.getElementById('content')
+    // let content = document.getElementById('content')
     
     const addTitle = () => {    
-        let title = document.createElement('div')
-        title.classList.add('title-element')
-        title.innerText = 'popa'
-
+        // let title = document.createElement('div')
+        // title.classList.add('title-element')
+        // title.innerText = 'popa'
+        console.log('Hello!')
         
-        content.appendChild(title)
+        // content.appendChild(title)
 
     }
 
